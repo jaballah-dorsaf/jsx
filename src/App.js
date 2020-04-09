1d4fb284d85ea280e675927040d93e1773d161aa
jaballah-dorsaf <jaballahdorsaf9@gmail.com>
2020-04-09 14:33:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( <div className = "App" >
+    <h1 className = "titlered" > Your name here </h1>    
+    <img src = "/imageInPublic.jpg" />
+    <img src = "/imageInSrc.jpg" />
     </div>
   );
 }
