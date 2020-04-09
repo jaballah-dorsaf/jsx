67edeaf67a93ceb9230b385dbb975e141b115a71
jaballah-dorsaf <jaballahdorsaf9@gmail.com>
2020-04-09 13:41:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
-  return ( <div className = "App" >
-    <h1 className = "titlered" > Your name here </h1>    
-    <img src = "/imageInPublic.jpg" />
-    <img src = "/imageInSrc.jpg" />
-    </div>
+  return ( < div className = "App" >
+    <
+    h1 className = "title red" > Your name here < /h1> <
+    img src = "/imageInPublic.jpg" / >
+    <
+    img src = "/imageInSrc.jpg" / >
+    <
+    /div>
   );
 }
 
